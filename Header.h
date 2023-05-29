1645369d9a3bcb3a7695a5b7918f519c9ff61461
Colnuwko <Vladislav290403@gmail.com>
2023-05-29 23:13:59 +0400

    Added Bellman Ford algorithm

--- Header.h
+++ Header.h
@@ -1,2 +1,274 @@
 #include <iostream>
 #include <stdio.h>
+#include <vector>
+#include <locale.h>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+
+
+template<typename Vertex = int>
+
+class Graph
+{
+public:
+    struct Edge
+    {
+        Vertex out, in;
+        double distance;
+        //Edge(Vertex out, Vertex in, double dis) : out(out), in(in), distance(dis) {};
+    };
+private:
+   
+    unordered_map<Vertex, vector<Edge>> data;
+
+public:
+    Graph(const vector<Vertex>& v)
+    {
+        for (auto& i : v)
+        {
+            add_vertex(i);
+        }
+    }
+    bool has_vertex(const Vertex& v) const
+    {
+        for (auto& i : data)
+        {
+            if (v == i.first) return true;
+        }
+        return false;
+    }
+    void add_vertex(const Vertex& v)
+    {
+        if (has_vertex(v)) cout << "\nТакая вершина уже существует\n";
+        else
+            data.insert({ v, {} });
+    }
+    bool remove_vertex(const Vertex& v)
+    {
+        if (!has_vertex(v))
+        {
+            cout << "\nТакой вершины не существует\n";
+            return false;
+        }
+        else
+        {
+            data.erase(v);
+            return true;
+        }
+    }
+    vector<Vertex> vertices() const//возвращаем список вершин
+    {
+        vector<Vertex> result;
+        for (auto& i : data)
+        {
+            result.push_back(i.first);
+        }
+        return result;
+    }
+   
+ 
+    void print_vertex()
+    {
+
+        cout << "Вершины" <<"\n";
+        for (auto& i : data )
+        {
+            cout << i.first <<" ";
+        }
+        cout << endl;
+    }
+
+    void add_edge(const Vertex& out, const Vertex& in)
+    {
+        if (!has_vertex(out)) { add_vertex(out); }
+        if (!has_vertex(in)) { add_vertex(in); }
+        data.find(out)->second.push_back({ out, in,NULL });
+    }
+    void add_edge(const Vertex& out, const Vertex& in, const double& dis)
+    {
+        if (!has_vertex(out)) { add_vertex(out); }
+        if (!has_vertex(in)) { add_vertex(in); }
+        data.find(out)->second.push_back({ out, in,dis });
+    }
+    void add_edge(const Edge& edge)
+    {
+        if (!has_vertex(edge.out)) { add_vertex(edge.out); }
+        if (!has_vertex(edge.in)) { add_vertex(edge.in); }
+        data.find(edge.out)->second.push_back({edge.out, edge.in,edge.distance });
+    }
+    void print_graph()
+    {
+        cout << "Вершина --> (Куда, расстояние)" << "\n";
+        for (auto& i : data)
+        {
+            cout << i.first << " --> ";// << i.second[0].in;
+            for (auto& j : i.second)
+            {
+                if (j.distance!=0)
+                {
+                    cout << "(" << j.in << "," << j.distance << ")";
+                }
+                else
+                {
+                    cout << "(" << j.in << ",None)";
+                }
+            }
+            cout << "\n";
+        }
+        cout << endl;
+    }
+    bool remove_edge(const Vertex& out, const Vertex& in)
+    {
+        if (!has_vertex(out) || !has_vertex(in)) return false;
+        auto& edges = data.find(out)->second;
+        auto it = edges.begin();
+        while (it != edges.end()) {
+            if (it->out == out and it->in == in)
+            {
+                it = edges.erase(it);
+            }
+            else ++it;
+        }
+        return true;
+    }
+    bool remove_edge(const Vertex& out, const Vertex& in, const double & dis)
+    {
+        if (!has_vertex(out) || !has_vertex(in)) return false;
+        auto& edges = data.find(out)->second;
+        auto it = edges.begin();
+        while (it != edges.end()) {
+            if (it->out && out && it->in == in && it->distance == dis)
+            {
+                it = edges.erase(it);
+            }
+            else ++it;
+        }
+        return true;
+    }
+    bool remove_edge(const Edge& edge)
+    {
+        if (!has_edge(edge)) return false;
+        auto& edges = data.find(edge.out)->second;
+        auto it = edges.begin();
+        while (it != edges.end()) {
+            if (it->out == edge.out and it->in == edge.in and it->distance ==edge.distance)
+            {
+                it = edges.erase(it);
+            }
+            else ++it;
+        }
+        return true;
+    }
+    bool has_edge(const Vertex& out, const Vertex& in) const
+    {
+      
+        for (auto& j : data.find(out)->second)
+        {
+            if (j.out == out && j.in == in)  return true;
+        }
+        
+        return false;
+    }
+    bool has_edge(const Edge& edge)
+    {
+        for (auto& j : data.find(edge.out)->second)
+        {
+            if (j.out == edge.out && j.in == edge.in && j.distance ==edge.distance)  return true;
+        }
+
+        return false;
+    }
+    vector<Edge> edges(const Vertex& out) const
+    {
+        if (!has_vertex(out))throw logic_error("Нет такой вершины");
+        auto i = data.find(out)->second;
+        return(data.find(out)->second);
+    }
+    size_t order() const
+    {
+        return data.size();
+    }//порядок
+    size_t degree(const Vertex& v) const
+    {
+        if (!has_vertex(v)) throw invalid_argument("Нет такой вершины!");
+        return edges(v).size();
+    }//степень
+    vector<Vertex>  walk(const Vertex& start_vertex)const 
+    {
+        //vector<Vertex> result;
+        vector<Vertex> used;
+        used.push_back(start_vertex); // массив использованных
+        queue<int> q;
+        q.push(start_vertex);
+        //result.push_back(start_vertex);
+        while (!q.empty()) {
+            auto ver = q.front();
+            vector<Edge> g = edges(ver);
+            q.pop();
+            for (int i = 0; i < g.size(); i++) {
+                Vertex v = g[i].in;
+                bool found = std::find(used.begin(), used.end(), v) != used.end();
+                if (!found)
+                {
+                    used.push_back(v);
+                    q.push(v);
+                    //result.push_back(v);
+                }
+            }
+        }
+        cout << endl;
+        for (auto& i : used)
+        {
+            cout << i<<"\n";
+        }
+        return used;
+    }
+    vector<Edge> Algoritm_of_Belman_ford(const Vertex& out, const Vertex& in) const
+    {
+        if (!has_vertex(out) || !has_vertex(in))throw std::invalid_argument("Вершина не найдена!");
+        vector<Edge> resullt;
+       /* vector<Edge> temp;*/
+        std::unordered_map<Vertex, double> distance;
+        std::unordered_map<Vertex, Edge> ancestors;
+        vector<Edge> parent;
+        parent.push_back({ out,out,0 });
+        //cout << parent[0].out << parent[0].in << parent[0].distance;
+        for (auto& item : data)
+        {
+            distance[item.first] = 100000000000;
+            distance[out] = 0;
+        }
+        //auto vec = vertices();
+        //for (auto& k : vec)
+        //{
+
+        //    for (auto& j : edges(k))
+        //    {
+        //        //cout << j.in;
+        //        temp.push_back(j);
+        //    }
+        //}
+        //for (auto& i : temp)
+        //{
+        //    cout << i.out << i.in << i.distance << "  ";
+        //}
+        for (size_t i = 0; i < order() - 1; i++)
+        {
+            for (const auto& pair : data)
+            {
+                for (const auto& edge : pair.second)
+                {
+                    if (distance[edge.out] + edge.distance < distance[edge.in])
+                    {
+                        distance[edge.in] = distance[edge.out] + edge.distance;
+                        ancestors[edge.in] = edge;
+                    }
+                }
+            }
+        }
+        cout << distance[in];
+    }
+};
--- Source.cpp
+++ Source.cpp
@@ -2,16 +2,70 @@
 
 
 
-
-
-
-
-
-
-
 void main()
 {
-	setlocale(LC_ALL, "Russia");
+    setlocale(LC_ALL, "Rus");
+    Graph<int> graph({ 1,4,6,3,2,7 });
 
+    graph.add_vertex(1);
+    graph.add_vertex(3);
+    graph.add_vertex(7);
+    graph.add_vertex(7);
+    graph.print_graph();
+    graph.add_edge(1, 4, 5);
+    graph.add_edge(1, 3, 1);
+    graph.add_edge(6, 4, 1);
+    graph.add_edge(3, 6, 4);
+    graph.add_edge(2, 7);
+    graph.add_edge(3, 2);
+    //graph.remove_edge(3, 2);
+    //graph.remove_edge(1,4,5);
+    graph.add_edge(1, 7, 5);
+    graph.add_edge(1, 3, 6);
+    graph.add_edge(4, 2, 4);
+    graph.add_edge(6, 6, 6);
+    graph.add_edge(2, 2);
+    graph.add_edge(3, 1);
+    graph.print_vertex();
+    graph.print_graph();
+    if (!graph.has_edge(2, 3)) cout << "p,,";
+    if (!graph.has_edge(1, 4)) cout << "p,,";
+    if (graph.has_edge(2, 7)) cout << "p,,";
+    if (graph.has_edge(3, 2)) cout << "p,,";
+    auto i = graph.edges(1);
+    graph.walk(1);
+    cout << "(" << 1 << "--->  ";
+    for (auto& j : i)
+    {
+        cout << j.in;
+    }
+    cout << ")";
+    graph.Algoritm_of_Belman_ford(1, 4);
+        // построить Graph
 
-}
+        //graph.print_adjacency_matrix();
+
+};
+//void print_adjacency_matrix()
+ //{
+ //    for (int i = 0; i < adjlist.size(); i++)
+ //    {
+ //        cout << i << " ---> ";// вершину и список смежных с ней
+ //        for (int v : adjlist[i]) {
+ //            cout << v << " ";
+ //        }
+ //        cout << endl;
+ //    }
+ //}
+    //bool contain_vertex(int number_of_vertex)
+    //{
+    //    for (auto& i : vertex)
+    //    {
+    //        if (i == number_of_vertex) return true;
+    //    }
+    //    return false;
+    //}
+    //void add_vertex(const Vertex& v)
+    //{
+    //    if (!contain_vertex(v))vertex.push_back(v);
+    //}
